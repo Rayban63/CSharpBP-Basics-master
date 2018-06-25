aa37a6ca33ed3a5d33e03a7980300f37faa25945
RayDev <rvdz_@DESKTOP-O0SEM5T>
2018-06-25 16:40:45 +0200

    Add product class including unit test

---
+++ AcmeApp/Tests/Acme.BizTests/ProductTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Acme.Biz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acme.Biz.Tests
+{
+    [TestClass()]
+    public class ProductTests
+    {
+        [TestMethod()]
+        public void SayHelloTest()
+        {
+            //--Arrange
+            var currentProduct = new Product();
+            currentProduct.ProductName = "Saw";
+            currentProduct.ProductId = 1;
+            currentProduct.ProductDescription = "15 inch Steal hand blade";
+            var expected = "Hello Saw (1): 15 inch Steal hand blade";
+
+            //--Act
+            var actual = currentProduct.SayHello();
+
+            //--Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
