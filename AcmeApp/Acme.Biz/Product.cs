37c968994fbc898ec6feae93629975b978861ad1
RayDev <rvdz_@DESKTOP-O0SEM5T>
2018-07-09 07:29:00 +0200

    modify code to string interpolation

--- AcmeApp/Acme.Biz/Product.cs
+++ AcmeApp/Acme.Biz/Product.cs
@@ -120,7 +120,7 @@ namespace Acme.Biz
         }
         internal  string Category { get; set; }
         public int SequenceNumber { get; set; } = 1;
-        public string ProductCode => this.Category + "-" + this.SequenceNumber;
+        public string ProductCode => $"{this.Category}-{ this.SequenceNumber}";
 
 
         public string ValidationMessage { get; private set; }
